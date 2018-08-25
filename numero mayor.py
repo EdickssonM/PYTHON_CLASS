10222f8caaefccd436a0cca9d5edb5abf0a142a6
edicksson_miramag <edisonm1233@gmail.com>
2018-08-25 17:26:42 -0500

    practica 2

---
+++ numero mayor.py
@@ -0,0 +1,15 @@
+# Numero mayor
+import os # Importar librerias del sistema
+
+os.system ( " cls " )
+
+n1=0
+n2=0
+n1=input("ingrese primer Numero: ")
+n2=input("ingrese segundo Numero: ")
+print ("*********************")
+if n1>n2:
+	print ("el numero mayor es:",n1)
+else :
+	print ("el numero mayor es ",n2) 
+
