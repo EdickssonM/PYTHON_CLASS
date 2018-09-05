54bda74736403a3e158ad9d6a47fe6abbf37b2d9
edicksson_miramag <edisonm1233@gmail.com>
2018-09-05 08:33:46 -0500

    practica3

---
+++ ciclo 3.py
@@ -0,0 +1,7 @@
+rango =1
+ran=int(input(".:ingrese el rango de la tabla a desarrollar:."))
+tab=int(input(".:ingrese el numero de la tabla a desarrollar.:"))
+while rango<=ran :
+    print (tab, " * " ,rango,"=",tab*rango)
+    rango=rango+1
+print ("**************************")
---
+++ ejercicio5.py
@@ -0,0 +1,3 @@
+tabi =int(input("ingrese el numero inicial del rango:."))
+tabmax=int(input("ingrese el numero final del rango"))
+print(list(range(tabi, tabmax + 1)))
