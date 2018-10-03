d94a488344ab2574e80a07950e2757a702058fb6
EdickssonM <edisonm1233@gmail.com>
2018-10-03 16:34:15 -0500

    ok

--- cajero automatico.py
+++ cajero automatico.py
@@ -1,5 +1,5 @@
 intentos=3
-saldo=5000000
+saldo=2500
 s=1
 while s <= intentos:
     clave=int(input (" ingrese su clave:  "))
@@ -13,11 +13,12 @@ while s <= intentos:
         print ("4. Salir")
         print ("   ")
 
-        opc=int(input (" Digite la operacion a realizar:"))
+        opc=int(input (" Digite la operacion a realizar: " ))
         if opc == 1 :
             print("cambiar su clave")
-            clavenueva = 1234
+            clavenueva=int(input("ingrese su clave nueva " ))
             print ("su clave nueva es", clavenueva)
+            break
         elif opc == 2:
             
             print ("su saldo de cuenta es", saldo)
@@ -36,58 +37,71 @@ while s <= intentos:
                 subop=int(input (" Digite la operacion a realizar:"))
                 if subop==1:
                     nuevosaldo=saldo-10000
-                    print ("ha retirado $10.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo)
                     if nuevosaldo<0:
                         print ("tiene fondos insuficientes en la cuenta")
+                    else:
+                        print ("ha retirado $10.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo)
+                    
                 elif subop==2:
                     nuevosaldo1=saldo-20000
-                    print ("ha retirado $20.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo1)
                     if nuevosaldo1<0:
                         print ("tiene fondos insuficientes en la cuenta")
+                    else:
+                        print ("ha retirado $20.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo1)
+                    
                 elif subop==3:
                     nuevosaldo2=saldo-50000
-                    print ("ha retirado $50.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo2)
                     if nuevosaldo2<0:
                         print ("tiene fondos insuficientes en la cuenta")
+                    else:
+                        print ("ha retirado $50.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo2)
+                    
                 elif subop==4:
                     nuevosaldo3=saldo-100000
-                    print ("ha retirado $100.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo3)
                     if nuevosaldo3<0:
                         print ("tiene fondos insuficientes en la cuenta")
+                    else:
+                        print ("ha retirado $100.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo3)
+                    
                 elif subop==5:
                     nuevosaldo4=saldo-200000
-                    print ("ha retirado $200.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo4)
                     if nuevosaldo4<0:
                         print ("tiene fondos insuficientes en la cuenta")
+                    else:
+                        print ("ha retirado $200.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo4)
                 elif subop==6:
                     nuevosaldo5=saldo-400000
-                    print ("ha retirado $400.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo5)
                     if nuevosaldo5<0:
                         print ("tiene fondos insuficientes en la cuenta")
+                    else:
+                        print ("ha retirado $400.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo5)
+                    
                 elif subop==7:
                     nuevosaldo6=saldo-600000
-                    print ("ha retirado $600.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo6)
                     if nuevosaldo6<0:
                         print ("tiene fondos insuficientes en la cuenta")
+                    else:
+                        print ("ha retirado $600.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo6)
                 elif subop==8:
                     nuevosaldo7=saldo-1000000
-                    print ("ha retirado $1.000.000 ")
-                    print ("su nuevo saldo es ",nuevosaldo7)
                     if nuevosaldo7<0:
                         print ("tiene fondos insuficientes en la cuenta")
-
+                    else:
+                        print ("ha retirado $1.000.000 ")
+                        print ("su nuevo saldo es ",nuevosaldo7)
+                    
                 break
                 
             else :
                 print ("tiene fondos insuficientes en la cuenta")
-    else:
-        print ("contraseña incorrecta,su tarjeta ha sido bloqueada por intentar 3 veces")
+else:
+    print ("contraseña incorrecta,su tarjeta ha sido bloqueada por intentar 3 veces")
 
 
