ecbfbc2d1a8d7bc2baadf206daebf938aa6626cb
EdickssonM <edisonm1233@gmail.com>
2018-12-05 22:59:12 -0500

    ok

--- proyecto final/botones.py
+++ proyecto final/botones.py
@@ -8,7 +8,8 @@ arduinoData = serial.Serial ('COM10',9600)
 
 def on_offLuces(valor):
         print ("encender/apagar")
-        arduinoData.write(b'valor')
+        #arduinoData.write(b'valor')
+        arduinoData.write(str.encode(valor))
 def luces():
 	luz=Tk()
 	luz.title("Luces")
@@ -16,11 +17,12 @@ def luces():
 	luz.configure (background="black")
 	luz.resizable(FALSE, FALSE)
 	print ("luces")
-	luces_Cocina = Button(luz, text="L_Cocina",width=10,height=3, command= lambda :on_offLuces(1)).place (x=10, y=40)
-	luces_Habitacion = Button(luz, text="L_Habitacion",width=10,height=3, command= lambda :on_offLuces(2)).place (x=110, y=40)
-	luces_Letrero = Button(luz, text="L_Letrero",width=10,height=3, command=lambda :on_offLuces(5)).place (x=210, y=40)
-	luces_Garaje = Button(luz, text="L_Garaje",width=10,height=3, command=lambda :on_offLuces(4)).place (x=310, y=40)
-	luces_Escalera = Button(luz, text="L_Escalera",width=10,height=3, command=lambda :on_offLuces(3)).place (x=150, y=120)
+	luces_Cocina = Button(luz, text="L_Cocina",width=10,height=3, command= lambda :on_offLuces('1')).place (x=10, y=40)
+	luces_Habitacion = Button(luz, text="L_Habitacion",width=10,height=3, command= lambda :on_offLuces('2')).place (x=110, y=40)
+	luces_Letrero = Button(luz, text="L_Letrero",width=10,height=3, command=lambda :on_offLuces('6')).place (x=210, y=40)
+	luces_Garaje = Button(luz, text="L_Garaje",width=10,height=3, command=lambda :on_offLuces('4')).place (x=310, y=40)
+	luces_Escalera = Button(luz, text="L_Escalera",width=10,height=3, command=lambda :on_offLuces('3')).place (x=170, y=120)
+	luces_Disco = Button(luz, text="L_discoteca",width=10,height=3, command=lambda :on_offLuces('5')).place (x=80, y=120)
         
 def todo():
        todo=Tk()
@@ -29,7 +31,7 @@ def todo():
        todo.resizable(FALSE, FALSE)
        todo.configure (background="yellow")
        print ("Encender y apagar todo")
-       todo = Button(todo, text="ON/OFF",width=5,height=3,command= lambda :on_offLuces(6)).place (x=10, y=60)
+       todo = Button(todo, text="ON/OFF",width=5,height=3,command= lambda :on_offLuces('7')).place (x=10, y=60)
 
 def servo1():
         servo=Tk()
@@ -38,7 +40,7 @@ def servo1():
         servo.resizable(FALSE, FALSE)
         servo.configure (background="green")
         print ("Abrir/Cerrar puerta")
-        PuertaGaraje = Button(servo, text="Garaje",width=15,height=3,command = lambda: lambda :on_offLuces(7)).place (x=10, y=60)
+        PuertaGaraje = Button(servo, text="Garaje",width=15,height=3,command = lambda:on_offLuces('8')).place (x=10, y=60)
 
 
 	
@@ -54,3 +56,4 @@ BotonTodo=Button(domotica,text="ON/OFF_Todo",width=30,height=3,command=todo).pla
 BotonServoG=Button(domotica,text="Puerta_Garaje",width=30,height=3,command=servo1).place(x=50,y=170)
 
 domotica.mainloop()
+arduinoData.close()
--- proyecto final/servo.ino/servo.ino.ino
+++ proyecto final/servo.ino/servo.ino.ino
@@ -2,30 +2,34 @@ char domotica;
 #include <Servo.h>
           Servo servoGaraje;//entrada del garaje
 #include <LiquidCrystal.h>
-          const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+          const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
         LiquidCrystal lcd(rs, en, d4, d5, d6, d7);//configuracion de puertos de la pantalla
 
 float tempC;
+float tempF;
 
 //asignacion de pines y led`s
 int luzCocina = 40;
-int luzHabitacion = 31;
-int luzEscalera = 26;
+int luzHabitacion = 32;
+int luzEscalera = 30;
 int luzGaraje = 42;
-int LedRojo = 48;
+int LedRojo = 44;
 int LedVerde = 46;
-int temperatura = 30;
-int sensorMovimiento = 32;
+int temperatura = A1;
+int luz_disco1 = 38;
+int luz_disco2 = 36;
+int luzLetrero = 48;
+int luzSala = 34;
 int Cocina_on=0;
 int habitacion_on=0;
 int escalera_on=0;
 int garaje_on=0;
 int todo_on=0;
 int servo_on=0;
-int luzLetrero = 48;
 int letrero_on=0;
 int pinServo = 0;
 int valorServo =0;
+int disco_on=0;
 
 
 void setup() {
@@ -33,11 +37,8 @@ void setup() {
 
   //configuracion de pantalla led
   //Definir las dimensiones del LCD (16x2)
-  //y los caracteres que deben salir en las lineas
   lcd.begin(16,2);
-  lcd.print("Temperatura");
-  lcd.setCursor(0,1);
-  lcd.print ("C=");
+
 
   //configuramos los pines de los led`s, lcd y sensores
   pinMode(LedRojo,OUTPUT);//sensor
@@ -47,9 +48,10 @@ void setup() {
   pinMode(luzEscalera, OUTPUT);
   pinMode(luzGaraje, OUTPUT);
   pinMode(luzLetrero,OUTPUT);
-
-  pinMode(temperatura, OUTPUT);
-  pinMode(sensorMovimiento, OUTPUT);
+  pinMode(luz_disco1,OUTPUT);
+  pinMode(luz_disco1,OUTPUT);
+  pinMode(luzSala,OUTPUT);
+  pinMode(temperatura, INPUT);
 
 // Iniciamos el servo para que empiece a trabajar con el pin 9
   servoGaraje.attach(9);
@@ -58,26 +60,45 @@ void setup() {
 }
  
 void loop() {
-   if(Serial.available()>0) 
-   {
-    domotica=Serial.read();
-    Serial.print(domotica);
-  }
-  {servoGaraje.write (90);
-  valorServo=analogRead (pinServo);
-  Serial.println(valorServo);
-  if(valorServo <20)
-  {
-    servoGaraje.write (0);
-  }
-  delay(100);
-  }
-    {tempC = analogRead (temperatura);
+    //Servo Fotocelda####################
+    servoGaraje.write (90);
+    valorServo=analogRead (pinServo);
+    Serial.println(valorServo);
+    digitalWrite (LedRojo,HIGH);
+    digitalWrite (LedVerde,LOW);
+    if(valorServo < 20){
+      servoGaraje.write (0);
+      delay(100);
+      digitalWrite (LedVerde,HIGH);
+      digitalWrite (LedRojo,LOW);
+       lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
+       lcd.print("puerta del garaje"); //mensaje superior
+       lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
+       lcd.print (" ABIERTA =0 "); //mensaje inferior }
+       {delay(5000);
+          lcd.clear();}}
+    
+    //Temperatura ########################
+    tempC = analogRead (A1);
     tempC = (5.0 * tempC * 100) / 1024.0;
     Serial.println (tempC);
+    tempF = (tempC * 1.8) + 32;
+    lcd.setCursor(2, 0);
+    lcd.print("Temperatura");
+
+    lcd.setCursor(0, 1);
+    lcd.print(tempC);   //Muestra en la pantalla la temperatura en grados Centígrados
+    lcd.print(" C  ");
+    lcd.print(tempF);    //Muestra en la pantalla la temperatura en grados Fahrenheit
+    lcd.print(" F  ");
     delay (1000);
-     }
-    
+
+    //####################################
+  
+  if(Serial.available()>0) 
+   {
+    domotica=Serial.read();
+    Serial.print(domotica);    
       if (domotica == '1')
       { if(Cocina_on==0)
         { digitalWrite(luzCocina, HIGH);
@@ -85,6 +106,8 @@ void loop() {
           lcd.print("luz de la Cocina"); //mensaje superior
           lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
           lcd.print (" encendida =) "); //mensaje inferior
+          {delay(5000);
+          lcd.clear();}
           Cocina_on=1;
         }else
         { digitalWrite(luzCocina, LOW);
@@ -92,8 +115,9 @@ void loop() {
           lcd.print("luz de la Cocina"); //mensaje superior
           lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
           lcd.print (" apagada =( "); //mensaje inferior
-          Cocina_on=0;
-        }
+          {delay(5000);
+          lcd.clear();}
+          Cocina_on=0;}
       }else{
             if (domotica == '2')
                { if (habitacion_on==0)
@@ -102,6 +126,8 @@ void loop() {
                  lcd.print("luz de la Habitacion"); //mensaje superior
                  lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                  lcd.print (" encendida =) "); //mensaje inferior
+                 {delay(5000);
+                  lcd.clear();}
                  habitacion_on=1;
                 }else
                 {digitalWrite(luzHabitacion, LOW);
@@ -109,6 +135,8 @@ void loop() {
                  lcd.print("luz de la Habitacion"); //mensaje superior
                  lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                  lcd.print (" apagada =( "); //mensaje inferior
+                 {delay(5000);
+                  lcd.clear();}
                  habitacion_on=0;
                  }
         }else{ 
@@ -119,14 +147,18 @@ void loop() {
                       lcd.print("luz de la escalera"); //mensaje superior
                       lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                       lcd.print (" encendida =) "); //mensaje inferior
-                      habitacion_on=1;
+                      {delay(5000);
+                       lcd.clear();}
+                      escalera_on=1;
                      }else
                      {digitalWrite(luzEscalera, LOW);
                       lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
                       lcd.print("luz de la escalera"); //mensaje superior
                       lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                       lcd.print (" apagada =( "); //mensaje inferior
-                      habitacion_on=0;
+                      {delay(5000);
+                       lcd.clear();}
+                      escalera_on=0;
                      }
            }else{
                if (domotica == '4')
@@ -136,6 +168,8 @@ void loop() {
                        lcd.print("luz del Garaje"); //mensaje superior
                        lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                        lcd.print (" encendida =) "); //mensaje inferior
+                       {delay(5000);
+                        lcd.clear();}
                        garaje_on=1;
                        }else
                        {digitalWrite(luzGaraje, LOW);
@@ -143,16 +177,43 @@ void loop() {
                         lcd.print("luz del Garaje"); //mensaje superior
                         lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                         lcd.print (" apagada =( "); //mensaje inferior
+                        {delay(5000);
+                         lcd.clear();}
                         garaje_on=0;
                        }
            }else{
-               if (domotica == '5')
+                  if (domotica == '5')
+                    {if(disco_on==0)
+                         {digitalWrite(luz_disco1,HIGH);
+                          digitalWrite(luz_disco2,HIGH);
+                          lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
+                          lcd.print("luz de la disco"); //mensaje superior
+                          lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
+                          lcd.print (" encendida =) "); //mensaje inferior
+                          {delay(5000);
+                          lcd.clear();}
+                          disco_on=1;
+                          }else
+                          {digitalWrite(luz_disco1,LOW);
+                          digitalWrite(luz_disco2,LOW);
+                          lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
+                          lcd.print("luz de la disco"); //mensaje superior
+                          lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
+                          lcd.print (" apagada =( "); //mensaje inferior
+                          {delay(5000);
+                          lcd.clear();}
+                          disco_on=0;
+                       }
+           }else{
+               if (domotica == '6')
                   {if(letrero_on==0)
                       {digitalWrite(luzLetrero, HIGH);
                        lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
-                       lcd.print("luz Letrero"); //mensaje superior
+                       lcd.print("  luz Letrero  "); //mensaje superior
                        lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                        lcd.print (" encendida =) "); //mensaje inferior
+                       {delay(5000);
+                        lcd.clear();}
                        letrero_on=1;
                        }else
                        {digitalWrite(luzLetrero, LOW);
@@ -160,55 +221,77 @@ void loop() {
                         lcd.print("luz Letrero"); //mensaje superior
                         lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                         lcd.print (" apagada =( "); //mensaje inferior
+                        {delay(5000);
+                        lcd.clear();}
                         letrero_on=0;
                        }
          }else{
-              if (domotica == '6')
+              if (domotica == '7')
                  {if(todo_on==0)
                     {digitalWrite(luzCocina,HIGH);
                      digitalWrite(luzHabitacion,HIGH);
                      digitalWrite(luzEscalera,HIGH);
                      digitalWrite(luzGaraje,HIGH);
+                     digitalWrite(luzLetrero,HIGH);
+                     digitalWrite(luz_disco1,HIGH);
+                     digitalWrite(luz_disco2,HIGH);
+                     digitalWrite(luzSala,HIGH);
                      lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
                      lcd.print("    luces   "); //mensaje superior
                      lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                      lcd.print ("ENCENDIDAS =) "); //mensaje inferior
+                     {delay(5000);
+                     lcd.clear();}
                      todo_on=1;
                      }else
                      {digitalWrite(luzCocina,LOW);
                       digitalWrite(luzHabitacion,LOW);
                       digitalWrite(luzEscalera,LOW);
                       digitalWrite(luzGaraje,LOW);
+                      digitalWrite(luzLetrero,LOW);
+                      digitalWrite(luz_disco1,LOW);
+                      digitalWrite(luz_disco2,LOW);
+                      digitalWrite(luzSala,LOW);
                       lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
                       lcd.print("    luces   "); //mensaje superior
                       lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                       lcd.print ("APAGADAS =( "); //mensaje inferior
+                      {delay(5000);
+                       lcd.clear();}
                       todo_on=0;
                       }
          }else{ 
-               if (domotica == '7')
+               if (domotica == '8')
                   {if(servo_on==0)//servo garaje abrir
                       {servoGaraje.write (0);//posicion de servo en abrir
-                       delay(1000); //DAMOS UN TIEMPO
                        lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
                        lcd.print("puerta del garaje"); //mensaje superior
                        lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
                        lcd.print (" ABIERTA =0 "); //mensaje inferior 
                        digitalWrite (LedVerde,HIGH);
+                       digitalWrite (LedRojo,LOW);
+                       {delay(5000);
+                        lcd.clear();}
                        servo_on=1;
                       }else //servo garaje cerrar
                       {servoGaraje.write (90); // posicion de servo en cerrar
-                       delay(1000);
                        lcd.setCursor (0,0);//ubicacion del mensaje en la parte superior
                        lcd.print("Puerta del Garaje"); //mensaje superior
                        lcd.setCursor (0,1);//ubicacion del mensaje en la parte inferior
-                       lcd.print (" CERRADA =0 ");} //mensaje inferior
+                       lcd.print (" CERRADA =0 "); //mensaje inferior
                        digitalWrite (LedRojo,HIGH);
-                       }
+                       digitalWrite (LedVerde,LOW);
+                       {delay(5000);
+                        lcd.clear();}
+                       servo_on=0;
+                      }
+                  }
          }
          }
+           }
            }
            }
         }
       }
+   }
 }
